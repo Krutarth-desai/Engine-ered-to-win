818307b6eee166f2b36f6a8bcc3dad7d7f7240c6
Krutarth-desai <krutarthdesai66@gmail.com>
2026-09-03 11:57:45 +0530

    Updated UI, debugged display errors

--- frontend/src/components/DigitalTwinCenterpiece.tsx
+++ frontend/src/components/DigitalTwinCenterpiece.tsx
@@ -217,7 +217,7 @@ export default function DigitalTwinCenterpiece({
           <svg
             className="uav-svg-model"
             id="uav-svg"
-            viewBox="0 0 540 270"
+            viewBox="-10 -5 560 280"
             xmlns="http://www.w3.org/2000/svg"
             style={{ transform: svgTransform }}
           >
